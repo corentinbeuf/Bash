14f133d04c12e0fab34f48e5bc97d50ff6fb5bb6
Corentin BEUF <corentin.beuf.professionnel@proton.me>
2025-02-22 19:52:38 +0100

    Create Bash script to install Docker

--- Docker/install_docker.sh
+++ Docker/install_docker.sh
@@ -2,32 +2,32 @@
 echo "-------------------------"
 echo " Update packages/sources "
 echo "-------------------------"
-apt-get update -y > /dev/null
+sudo apt-get update -y > /dev/null
 echo "--------------------------------"
 echo " Install requierements packages "
 echo "--------------------------------"
-apt-get install ca-certificates curl gnupg -y > /dev/null
+sudo apt-get install ca-certificates curl gnupg -y > /dev/null
 echo "---------------------------"
 echo " Create folder for GPG key "
 echo "---------------------------"
-install -m 0755 -d /etc/apt/keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 echo "-------------------------"
 echo " Download Docker GPG key "
 echo "-------------------------"
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "-------------------"
 echo " Setup permissions "
 echo "-------------------"
-chmod a+r /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "-------------------"
 echo " Add Docker source "
 echo "-------------------"
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "-------------------------"
 echo " Update packages/sources "
 echo "-------------------------"
-apt-get update -y  > /dev/null
+sudo apt-get update -y  > /dev/null
 echo "-------------------------"
 echo " Install Docker packages "
 echo "-------------------------"
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y > /dev/null
